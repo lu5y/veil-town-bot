b70ea4e9ea7a906aed9748176b094748853cfa28
lu5y <156009394+lu5y@users.noreply.github.com>
2026-01-01 10:15:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# Environment variables
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_TOKEN_HERE")
+TEST_MODE = os.getenv("TEST_MODE", "True").lower() == "true"
+
+# Mechanics
+MIN_PLAYERS = 1 if TEST_MODE else 5
